3047838a3dff93ebb110972208a130af05b1b75b
CarolinaHV <carolina.hv02@gmail.com>
2021-10-11 13:06:14 -0500

    Add imagens and icons

--- frontend/src/components/shared/Menu.jsx
+++ frontend/src/components/shared/Menu.jsx
@@ -1,22 +1,30 @@
 import '../../styles/Menu.css';
+import img_home from '../../assets/img/icons/icons8-home-64.png';
+import order_img from '../../assets/img/icons/icons8-order-history-64.png';
+import user_img from '../../assets/img/icons/icons8-user-60.png';
 
 const Menu = () => {
   return (
+    <>
     <div className="menu">
       <nav>
         <ul>
           <li>
+            <img src={img_home} alt="home-icon"></img>
             <a href="./">HOME</a>
           </li>
           <li>
+            <img src={order_img} alt="home-icon"></img>
             <a href="/Order">Orders</a>
           </li>
           <li>
+            <img src={user_img} alt="home-icon"></img>
             <a href="/User">Users</a>
           </li>
         </ul>
       </nav>
     </div>
+    </>
   );
 }
 
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,5 +1,13 @@
+import '../styles/Home.css';
+import welcome from '../assets/img/reception.png';
+
 const Home = () => {
-    return <h1>WELCOME TO THE SYSTEM</h1>;
+    return (
+        <>
+        <h1>WELCOME</h1>
+        <img src={welcome} alt="women-in-reception"></img>
+        </>
+    );
 }
 
 export default Home;
